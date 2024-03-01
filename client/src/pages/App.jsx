641c9e7b18b2075a2595edea20eeda7058e7e530
hhman24 <anhoang483@gmail.com>
2024-03-01 16:12:04 +0700

    init prettier-plugin-tailwindcss

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className='text-2xl text-rose-600'>Hello</div>;
+  return <div className='p-4 text-2xl text-rose-600 '>Hello</div>;
 }
 
 export default App;
