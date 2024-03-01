be10c50adf0cd19c8d0f200cc5465a4fc04250e1
hhman24 <anhoang483@gmail.com>
2024-03-01 16:41:18 +0700

    init prettier eslint

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/test', (req, res) => {
-    res.json('test ok');
+app.get('/', (req, res) => {
+  res.sendStatus(200);
 });
 
-app.listen(4000);
+app.listen(4000, () => console.log('Application is running at port 4000'));
