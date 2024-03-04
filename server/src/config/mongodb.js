61288dac99f316c962573e9dcc4682c91bf88533
hhman24 <anhoang483@gmail.com>
2024-03-05 02:18:32 +0700

    hhman - setup mongodb

--- server/src/config/mongodb.js
+++ server/src/config/mongodb.js
@@ -1,3 +1,40 @@
 /**
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+import dotenv from 'dotenv';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+dotenv.config();
+
+// khởi tạo đối tượng chattingSystemInstance đến database ban đầu là null
+let chattingSystemInstance = null;
+
+// khởi tạo một đối tượng mongoClientInstance để kết nối mongodb
+const mongoClientInstance = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+});
+
+// kết nối database
+export const CONNECT_DB = async () => {
+  // Gọi kết nối tới MongoDB với URL đã khai báo trong mongoClientInstance
+  await mongoClientInstance.connect();
+
+  chattingSystemInstance = mongoClientInstance.db(process.env.DATABASE_NAME);
+};
+
+// Đóng kết nối với database khi cần
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close();
+};
+
+// Function Get_DB không async này có nhiệm vụ export ra cái chattingSystemInstance sau khi đã connect thành công tới mongodb để tái sử dụng nhìu nơi khác nhau trong code.
+// Note: lưu ý phải đảm bảo chỉ luôn gọi cái GET_DB này sau khi đã kết nối thành công tới mongodb
+export const GET_DB = () => {
+  if (!chattingSystemInstance)
+    throw new Error('Must connect to Database first');
+  return chattingSystemInstance;
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,34 +1,59 @@
+// eslint-disable no-console
+
 /**
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
-
+import dotenv from 'dotenv';
+import exitHook from 'async-exit-hook';
 import express from 'express';
-import { mapOrder } from '~/utils/sorts.js';
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb';
 
-const app = express();
+dotenv.config();
 
-const hostname = 'localhost';
-const port = 4000;
+const START_SERVER = () => {
+  const app = express();
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(
-    mapOrder(
-      [
-        { id: 'id-1', name: 'One' },
-        { id: 'id-2', name: 'Two' },
-        { id: 'id-3', name: 'Three' },
-        { id: 'id-4', name: 'Four' },
-        { id: 'id-5', name: 'Five' }
-      ],
-      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1']
-    )
-  );
-  res.end('<h1>Hello World!</h1><hr>');
-});
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray());
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`);
-});
+    res.end('<h1>Hello World!</h1><hr>');
+  });
+
+  app.listen(process.env.APP_PORT, () => {
+    console.log(
+      `3. Server is running on ${process.env.APP_HOST}:${process.env.APP_PORT}/`
+    );
+  });
+
+  // thực hiện các tác vụ cleanup trk khi dừng server
+  // link: https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
+  exitHook(() => {
+    console.log('4. Disconnecting from MongoDB...');
+    CLOSE_DB();
+    console.log('5. Disconnected from MongoDB');
+  });
+};
+
+// (IIFE) Immediately-invoke / Anomynous Async function
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB...');
+    await CONNECT_DB();
+    console.log('2. Connected to MongoDB Cloud Atlas');
+
+    START_SERVER();
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+})();
+
+// console.log('1. Connecting to MongoDB ...');
+// // Chỉ khi kết nối tới mongodb thành công thì mới START_SERVER lên
+// CONNECT_DB()
+//   .then(() => console.log('2. Connected to MongoDB Cloud Atlas'))
+//   .then(() => START_SERVER())
+//   .catch((error) => {
+//     console.log(error);
+//     process.exit(0);
+//   });
