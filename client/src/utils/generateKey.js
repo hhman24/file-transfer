332a65d68ae244cd3595919657663eb4779697ab
tienhoangggg <20127424@student.hcmus.edu.vn>
2024-03-22 22:51:39 +0700

    generate key

--- client/src/utils/generateKey.js
+++ client/src/utils/generateKey.js
@@ -1,1 +1,25 @@
-// Generate key publick and private
+const crypto = require('crypto');
+const keyLength = 2048;
+
+function generateKey() {
+    const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+        modulusLength: keyLength,
+        publicKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+        }
+    });
+    const aesKey = crypto.randomBytes(32).toString('hex');
+    const encryptedAesKey = crypto.publicEncrypt(publicKey, Buffer.from(aesKey, 'hex')).toString('hex');
+    return {
+        publicKey: publicKey,
+        privateKey: privateKey,
+        encryptedAesKey: encryptedAesKey
+    };
+}
+
+module.exports = generateKey;
