df960e3e97131310c5d3ee4a3d4444a61dd5869d
tienhoangggg <20127424@student.hcmus.edu.vn>
2024-03-27 23:07:36 +0700

    hehe

--- client/src/utils/generateKey.js
+++ client/src/utils/generateKey.js
@@ -1,7 +1,7 @@
-const crypto = require('crypto');
+import crypto from 'crypto';
 const keyLength = 2048;
 
-function generateKey() {
+function generateRSAKey() {
     const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
         modulusLength: keyLength,
         publicKeyEncoding: {
@@ -13,14 +13,13 @@ function generateKey() {
             format: 'pem'
         }
     });
-
     return {
         publicKey,
         privateKey
     };
 }
 
-function generateSymmetricKey(publicKey) {
+function generateAESKey(publicKey) {
     const symmetricKey = crypto.randomBytes(32).toString('hex');
     const encryptedSymmetricKey = crypto.publicEncrypt(publicKey, Buffer.from(symmetricKey, 'hex')).toString('hex');
     return {
@@ -28,13 +27,43 @@ function generateSymmetricKey(publicKey) {
     };
 }
 
-function decryptSymmetricKey(encryptedSymmetricKey, privateKey) {
+function decryptAESKey(encryptedSymmetricKey, privateKey) {
     const symmetricKey = crypto.privateDecrypt(privateKey, Buffer.from(encryptedSymmetricKey, 'hex')).toString('hex');
     return symmetricKey;
 }
 
-module.exports = {
-    generateKey,
-    generateSymmetricKey,
-    decryptSymmetricKey
+function encryptData(data, symmetricKey) {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(symmetricKey, 'hex'), iv);
+    let encrypted = cipher.update(data);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+function decryptData(data, symmetricKey) {
+    const parts = data.split(':');
+    const iv = Buffer.from(parts[0], 'hex');
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(symmetricKey, 'hex'), iv);
+    let decrypted = decipher.update(Buffer.from(parts[1], 'hex'));
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+//test
+// const { publicKey, privateKey } = generateRSAKey();
+// const { encryptedSymmetricKey } = generateAESKey(publicKey);
+// const symmetricKey = decryptAESKey(encryptedSymmetricKey, privateKey);
+// const data = 'Hello World';
+// const encryptedData = encryptData(data, symmetricKey);
+// const decryptedData = decryptData(encryptedData, symmetricKey);
+// console.log('Original data:', data);
+// console.log('Encrypted data:', encryptedData);
+// console.log('Decrypted data:', decryptedData);
+
+export const generateKey = {
+    generateRSAKey,
+    generateAESKey,
+    decryptAESKey,
+    encryptData,
+    decryptData
 };
